ec8b4172f09c26dd12b7e4fa914f9ddf2683c029
Maciej Cieplucha <snakeeyesplay@gmail.com>
2025-07-29 03:44:11 +0200

    Add: Implement sorting in TaskViewModel and UI

--- PMS.Presentation/ViewModels/TaskViewModel.cs
+++ PMS.Presentation/ViewModels/TaskViewModel.cs
@@ -14,6 +14,30 @@ namespace PMS.Presentation.ViewModels
         public ObservableCollection<TaskItemViewModel> Tasks { get; }
             = new ObservableCollection<TaskItemViewModel>();
 
+        // 1. Opcje sortowania
+        public ObservableCollection<string> SortOptions { get; }
+            = new ObservableCollection<string>
+        {
+            "Tytuł A-Z",
+            "Tytuł Z-A",
+            "Priorytet ↑",
+            "Priorytet ↓",
+            "Data newest",
+            "Data oldest"
+        };
+
+        private string _selectedSortOption;
+        // 2. Właściwość wyzwalająca sortowanie
+        public string SelectedSortOption
+        {
+            get => _selectedSortOption;
+            set
+            {
+                if (SetProperty(ref _selectedSortOption, value))
+                    ApplySorting();
+            }
+        }
+
         public IAsyncCommand AddCommand { get; }
 
         public TaskViewModel(
@@ -25,7 +49,28 @@ namespace PMS.Presentation.ViewModels
 
             AddCommand = new AsyncCommand(_ => OpenAddAsync());
 
+            SelectedSortOption = SortOptions.First();
             _ = LoadAsync();
+        }
+
+        private void ApplySorting()
+        {
+            IEnumerable<TaskItemViewModel> sorted = SelectedSortOption switch
+            {
+                "Tytuł A-Z" => Tasks.OrderBy(t => t.Title),
+                "Tytuł Z-A" => Tasks.OrderByDescending(t => t.Title),
+                "Priorytet ↑" => Tasks.OrderBy(t => t.Priority),
+                "Priorytet ↓" => Tasks.OrderByDescending(t => t.Priority),
+                "Data newest" => Tasks.OrderByDescending(t => t.StartDate),
+                "Data oldest" => Tasks.OrderBy(t => t.StartDate),
+                _ => Tasks
+            };
+
+            // Przebuduj ObservableCollection
+            var list = sorted.ToList();
+            Tasks.Clear();
+            foreach (var vm in list)
+                Tasks.Add(vm);
         }
 
         private async Task LoadAsync()
@@ -38,18 +83,22 @@ namespace PMS.Presentation.ViewModels
                 itemVm.Deleted += vm => Tasks.Remove(vm);
                 Tasks.Add(itemVm);
             }
+
+            // Po załadowaniu danych od razu sortujemy
+            ApplySorting();
         }
 
         private async Task OpenAddAsync()
         {
             var vm = new AddTaskViewModel(_repo, _dialogs);
-            var result = await _dialogs.ShowDialogAsync(vm);
-            if (result && vm.CreatedTask is TaskModel created)
-            {
-                var itemVm = new TaskItemViewModel(created, _repo, _dialogs);
-                itemVm.Deleted += vm => Tasks.Remove(vm);
-                Tasks.Add(itemVm);
-            }
+            var ok = await _dialogs.ShowDialogAsync(vm);
+            if (!ok || vm.CreatedTask is not TaskModel created) return;
+
+            var itemVm = new TaskItemViewModel(created, _repo, _dialogs);
+            itemVm.Deleted += vm => Tasks.Remove(vm);
+            Tasks.Add(itemVm);
+
+            ApplySorting();
         }
     }
 }
